c7dd64b3da8c8db50c00d09fc7432b487c3eac6f
Matthew McConaghie <matthewmcconaghie@MacBook-Pro.domain>
2017-10-19 09:07:20 -0700

    made changes to api routes and survey page

---
+++ app/data/friends.js
@@ -0,0 +1,38 @@
+var friends = [{
+        "name": "Kevin Bacon",
+        "photo": "https://vignette.wikia.nocookie.net/xmenmovies/images/5/53/Kevin-Bacon.jpg/revision/latest?cb=20120224061314",
+        "scores": [5, 2, 1, 4, 1, 3, 2, 4, 5, 1]
+    },
+    {
+        "name": "Taylor Swift",
+        "photo": "http://www.billboard.com/files/styles/article_main_image/public/media/taylor-swift-2014-sarah-barlow-billboard-650.jpg",
+        "scores": [1, 4, 5, 1, 5, 4, 2, 3, 5, 1]
+    },
+    {
+        "name": "Steve Nash",
+        "photo": "https://www.gannett-cdn.com/-mm-/286211da30a75f11e7a0ec5b42a3684aa2175ade/c=0-13-2285-3060&r=537&c=0-0-534-712/local/-/media/2017/04/07/USATODAY/USATODAY/636271604815222620-XXX-LAKERS-SUNS-STEVE-NASH-0117-15479591.JPG",
+        "scores": [5, 2, 3, 4, 1, 3, 1, 4, 3, 1]
+    },
+    {
+        "name": "George Bush",
+        "photo": "https://upload.wikimedia.org/wikipedia/commons/d/d4/George-W-Bush.jpeg",
+        "scores": [1, 2, 1, 2, 2, 1, 2, 2, 1, 1]
+    },
+    {
+        "name": "Johnny Depp",
+        "photo": "https://pbs.twimg.com/profile_images/461110955082924033/syKzaNQA.png",
+        "scores": [5, 2, 3, 2, 4, 1, 3, 2, 4, 5]
+    },
+    {
+        "name": "Eli Whitney",
+        "photo": "http://porterbriggs.com/wp-content/uploads/2013/06/eliwhitneycolor-e1482179956357.jpg",
+        "scores": [5, 2, 1, 5, 2, 2, 3, 2, 2, 5]
+    },
+    {
+        "name": "Brad Pit",
+        "photo": "http://pmcdeadline2.files.wordpress.com/2011/08/pitt_20110815213700.jpg",
+        "scores": [5, 3, 5, 4, 3, 3, 1, 4, 5, 2]
+    }
+]
+
+module.exports = friends;
